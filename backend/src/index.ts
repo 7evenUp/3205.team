c08bcd3664b15292fb37f0b24b0c317658edc267
Artyom 7evenUp <sheludeshev.artyom@mail.ru>
2026-06-24 17:34:39 +0300

    Добавил обработку CORS

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,11 @@
 import express from "express"
+import cors from "cors"
 
 import { router } from "./router/index.js"
 
 const app = express()
 
+app.use(cors({ origin: "http://localhost:5173" }))
 app.use(express.json())
 
 app.use("/api", router)
