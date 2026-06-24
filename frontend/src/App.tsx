00a542eb89cbd302f9297d3eaf25bfb4652e308f
Artyom 7evenUp <sheludeshev.artyom@mail.ru>
2026-06-24 16:39:54 +0300

    init frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,21 @@
+const App = () => {
+  return (
+    <div className="min-h-screen w-screen bg-zinc-950 text-white">
+      <div className="container mx-auto bg-zinc-900 py-12 px-6">
+        <header>
+          <h1 className="font-medium text-2xl">
+            3205.TEAM Test Task completion by 7evenUp
+          </h1>
+        </header>
+
+        <main>Основной контейнер</main>
+
+        <footer className="border-t-2 border-zinc-950 mt-20 pt-6">
+          Просто заглушка
+        </footer>
+      </div>
+    </div>
+  )
+}
+
+export default App
